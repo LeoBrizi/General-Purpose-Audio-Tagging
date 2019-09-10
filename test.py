40980262f4e5c772daad6936ff7440ccc65f5cf7
gabriele sellani <sellani.1709058@studenti.uniroma1.it>
2019-09-10 18:25:40 +0200

    plotting confusion matrix

--- test.py
+++ test.py
@@ -3,7 +3,65 @@ from CNNSpecNetwork import *
 import numpy as np
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
 import argparse
+import matplotlib.colors as colors
 from tqdm import tqdm
+
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
+
+def plot_confusion_matrix(y_true, y_pred, classes,
+                          normalize=False,
+                          title=None,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    #classes = classes[unique_labels(y_true, y_pred)]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=90, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    fig.set_size_inches(18.5, 10.5)
+    return ax
 
 
 def main(model_name, spec_version=1):
@@ -23,7 +81,10 @@ def main(model_name, spec_version=1):
     y_pred_arr = np.array(y_pred)
 
     confusionMatrix = confusion_matrix(y_test_arr, y_pred_arr)
-
+    a=[str(i) for i in range(1,42)]
+    plot_confusion_matrix(y_test_arr, y_pred_arr, classes=a,
+                      title='Confusion matrix, without normalization')
+    plt.show()
     print('Accuracy:')
     print(accuracy_score(y_test_arr, y_pred_arr))
     print('Precision:')
